655363542a2d30f15cbb98d3892d3dd69f58f2d0
Guilhermy Camargo <55157846+devguilhermy@users.noreply.github.com>
2020-02-12 11:32:31 -0300

    Settings panel created

---
+++ settings.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="card">
+		<div class="card-body">
+			<form>
+				<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+  <label class="form-check-label" for="defaultCheck1">
+    Group notes by pinned, archived and trashed
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+  <label class="form-check-label" for="defaultCheck1">
+    Display note footer only on hover
+  </label>
+</div>
+
+<div class="form-check">
+  <label class="form-check-label">
+    Select labels to not have its notes displayed
+  </label>
+</div>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
